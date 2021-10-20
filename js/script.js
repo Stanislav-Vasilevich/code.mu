de8417cbdd67e522d806151a99145100232ecd4c
Stanislav Vasilevich <stanislavasilevich@yandex.ru>
2021-10-20 20:32:33 +0300

    feat: add new note for tests js codes

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// 1
+/* 
+const num = 123;
+
+alert(num); 
+*/  
+
+// 2
+/* 
+const a = 1;
+const b = 2;
+const c = 3;
+const d = 4, e = 5;
+let f, j;
+f = 6;
+j = 7;
+
+console.log(a, b, c, d, e, f, j);
+*/ 
