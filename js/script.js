101f21a77dcef24e9ab58a574b4214bbbc77c7cd
Stanislav Vasilevich <stanislavasilevich@yandex.ru>
2021-10-20 21:38:31 +0300

    feat: выполнил - Числа и операции над ними в JavaScript

--- js/script.js
+++ js/script.js
@@ -17,3 +17,100 @@ j = 7;
 
 console.log(a, b, c, d, e, f, j);
 */ 
+
+// 3
+/*   
+let a = 10;
+a = 20;
+
+alert(a);
+*/
+
+// 4
+/*   
+const a = 1 + 2 + 3;
+
+alert(a);
+*/
+
+// 5
+/*   
+alert(1 + 2 + 3);
+*/
+
+// 6
+/*  
+const a = 10;
+const b = 2;
+
+alert(a + b);
+alert(a - b);
+alert(a * b);
+alert(a / b);
+*/
+
+// 7
+/*  
+const c = 10;
+const b = 5;
+const result = c + b;
+
+alert(result);
+*/
+
+// 8
+/*  
+const a = 1;
+const b = 2;
+const c = 3;
+
+alert(a + b + c);
+*/
+
+// 9
+/*  
+const a = 10;
+const b = 5;
+const c = a - b;
+const d = 7;
+const result = c + d;
+
+alert(result);
+*/
+
+// 10
+/*  
+const a = 1.5;
+const b = 0.75;
+
+alert(a + b);
+*/
+
+// 11
+/*  
+const a = -100;
+
+alert(a);
+*/
+
+// 12
+/*  
+const a = -10;
+
+alert(-a);
+*/
+
+// 13
+/*  
+const a = 10;
+const b = 3;
+
+alert(a % b);
+*/
+
+// 14
+/*  */
+alert(2 ** 10);
+
+// 15
+/*  */
