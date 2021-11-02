0430b2251c67f83d8ced4801dc02c47258c703cf
Stanislav <stanislavasilevich@gmail.com>
2021-11-02 20:59:55 +0300

    feat: выполнил задание: 'Константы в JavaScript'

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 // 1
-/* 
+/*
 const num = 123;
 
-alert(num); 
-*/  
+alert(num);
+*/
 
 // 2
-/* 
+/*
 const a = 1;
 const b = 2;
 const c = 3;
@@ -16,30 +16,30 @@ f = 6;
 j = 7;
 
 console.log(a, b, c, d, e, f, j);
-*/ 
+*/
 
 // 3
-/*   
+/*
 let a = 10;
 a = 20;
 
 alert(a);
 */
 
-// 4
-/*   
+// Числа и операции над ними в JavaScript
+/* 1
 const a = 1 + 2 + 3;
 
 alert(a);
 */
 
-// 5
-/*   
+// 2
+/*
 alert(1 + 2 + 3);
 */
 
-// 6
-/*  
+// 3
+/*
 const a = 10;
 const b = 2;
 
@@ -49,8 +49,8 @@ alert(a * b);
 alert(a / b);
 */
 
-// 7
-/*  
+// 4
+/*
 const c = 10;
 const b = 5;
 const result = c + b;
@@ -58,8 +58,8 @@ const result = c + b;
 alert(result);
 */
 
-// 8
-/*  
+// 5
+/*
 const a = 1;
 const b = 2;
 const c = 3;
@@ -67,8 +67,8 @@ const c = 3;
 alert(a + b + c);
 */
 
-// 9
-/*  
+// 6
+/*
 const a = 10;
 const b = 5;
 const c = a - b;
@@ -78,39 +78,126 @@ const result = c + d;
 alert(result);
 */
 
-// 10
-/*  
+// 7
+/*
 const a = 1.5;
 const b = 0.75;
 
 alert(a + b);
 */
 
-// 11
-/*  
+// 8
+/*
 const a = -100;
 
 alert(a);
 */
 
-// 12
-/*  
+// 9
+/*
 const a = -10;
 
 alert(-a);
 */
 
-// 13
-/*  
+// 10
+/*
 const a = 10;
 const b = 3;
 
 alert(a % b);
 */
 
-// 14
-/*  */
-alert(2 ** 10);
+// 11
+// alert(2 ** 10);
 
-// 15
-/*  */
+// Строки в JavaScript
+/* 1
+const str = '!!!';
+
+console.log('str: ', str);
+ */
+
+/* 2
+const a = 'java';
+const b = 'script';
+
+console.log(a + b);
+*/
+
+/* 3
+const a = 'hello';
+const b = 'world';
+
+console.log(a + ' ' + b);
+ */
+
+/* 4
+const str = 'Привет Мир программирования!))';
+
+console.log(str.length);
+*/
+
+/* 5
+const str = `a
+b
+c`;
+
+console.log(str);
+*/
+
+// Специальные значения в JavaScript
+/* 1
+let a;
+
+console.log(a);
+*/
+
+/* 2
+const a = null;
+
+console.log(a);
+*/
+
+/* 3
+const isTrue = true;
+
+console.log(isTrue);
+*/
+
+/* 4
+const isFalse = false;
+
+console.log(isFalse);
+*/
+
+/* 5
+const a = 'hello';
+const b = 'world';
+
+console.log(a * b);
+*/
+
+/* 6
+const a = 10;
+const b = 0;
+
+console.log(a / b);
+*/
+
+/* 7
+const a = -10;
+const b = 0;
+
+console.log(a / b);
+*/
+
+// Константы в JavaScript
+/* 1
+const PI = 3.14;
+const r = PI / 2;
+const d = r * 2;
+const l = PI * d ;
+
+console.log('l: ', l);
+*/
